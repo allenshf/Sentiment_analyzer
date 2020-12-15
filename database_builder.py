6f9e5a3b5bf82bdd942f75a8680acaeb00aba0eb
Allen Shufer <allenshufer@utexas.edu>
2020-12-15 12:40:50 -0600

    Most common words found, spell check added

--- database_builder.py
+++ database_builder.py
@@ -15,7 +15,7 @@ artists = results.find_all(class_='name')
 artists = [[artist.find('a').text, artist.find('a')['href']] for artist in artists]
 
 
-with open('reviews.csv', mode='w') as review_file:
+with open('reviews.csv', mode='w', newline='') as review_file:
 	review_writer = csv.writer(review_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
 	for artist in artists[:10]:
 		page = requests.get('https://www.songkick.com/' + artist[1])
@@ -25,6 +25,7 @@ with open('reviews.csv', mode='w') as review_file:
 		reviews = [review.find('p').text for review in results.find_all(class_='review-content')]
 		for review in reviews:
 			try:
-				review_writer.writerow([review])
+				if(len(review) > 0):
+					review_writer.writerow([review])
 			except UnicodeEncodeError:
 				print('Invalid character detected')
--- sentiment_analyzer.py
+++ sentiment_analyzer.py
@@ -1,20 +1,45 @@
 import nltk
-from nltk.corpus import movie_reviews
+import csv
+import string
+from symspellpy import SymSpell, Verbosity
+import pkg_resources
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+import pickle
 
-nltk.download('movie_reviews')
+with open('reviews_train.csv') as review_file:
+    reader = csv.reader(review_file, delimiter=',')
+    documents = [row[0] for row in reader]
 
-documents = [(list(movie_reviews.words(fileid)), category)
-              for category in movie_reviews.categories()
-              for fileid in movie_reviews.fileids(category)]
+result = []
+spell = SymSpell(max_dictionary_edit_distance=2,prefix_length=7)
+dictionary_path = pkg_resources.resource_filename(
+    "symspellpy", "frequency_dictionary_en_82_765.txt")
+spell.load_dictionary(dictionary_path, term_index=0, count_index=1)
 
-all_words = nltk.FreqDist(w.lower() for w in movie_reviews.words())
+for review in documents:
+    for word in review.split(' '):
+        if(len(word) > 0):
+            word = word.translate(str.maketrans('', '', string.punctuation))
+            try:
+                result.append(str(spell.lookup(word,Verbosity.CLOSEST, max_edit_distance=2)[0]).split(',')[0])
+            except IndexError:
+                result.append(word)
+
+all_words = nltk.FreqDist(word.lower() for word in result)
 word_features = list(all_words)[:2000]
 
 def document_features(document):
     document_words = set(document)
     features = {}
+    for word in document_words:
+        word = word.translate(str.maketrans('', '', string.punctuation))
+        try:
+            word = str(spell.lookup(word,Verbosity.CLOSEST, max_edit_distance=2)[0]).split(',')[0]
+        except IndexError:
+            word = word
     for word in word_features:
         features['contains({})'.format(word)] = (word in document_words)
     return features
 
+#TODO: Add sentiment column for training data
 featuresets = [(document_features(d), c) for (d,c) in documents]
